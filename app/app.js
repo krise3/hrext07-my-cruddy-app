f7a51d41901f854fb923a365a96ce82d032bdf80
Kristoffer Enriquez <enriquezkristoffer@yahoo.com>
2019-02-14 15:36:22 -0800

    Completed Sub-UI/X goal: "Create a data structure to hold a matrix representing notes to play at an interval"

--- app/app.js
+++ app/app.js
@@ -2,45 +2,34 @@
 Init app
 interact with DOM
 interact with localstorage
+*/
 
- */
+$(document).ready(function(){ // this is where we jquery
 
-$(document).ready(function(){
-  // this is where we jquery
+  var context = new AudioContext();
+
   //var keyData = 'ourKey'; // going to need to make this dynamic?
-
-
   $('.btn-add').on('click', function(e){
     console.log(e);
     var keyData = $('.input-key').val();
     var valueData = $('.input-value').val();
-    // write to db
-    localStorage.setItem(keyData, valueData);
-    // read from db
-    var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-    // this only displays the last one? might want to switch to html
-    // and append a div
-    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-    // if you use backticks ` you can use ${templateLiterals}
-    // TODO make this vars make sense across the app
-    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-    $('.input-key').val('');
-    $('.input-value').val('');
-  });
-
+    localStorage.setItem(keyData, valueData);                           // write to db
+    var displayText = keyData + ' | ' + localStorage.getItem(keyData);  // read from db                               // this only displays the last one? might want to switch to html
+    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');    // and append a div
+    $('.input-key').val('');                                                                                          // <div class="display-data-item" data-keyValue="keyData">valueData</div>
+    $('.input-value').val('');                                                                                        // if you use backticks ` you can use ${templateLiterals}
+  });                                                                                                                 // TODO make this vars make sense across the app
 
   // update db
-    // need to expand when  more than 1 item is added
-
-  // delete item
-  $('.container-data').on('click', '.display-data-item', function(e){
+  // need to expand when more than 1 item is added
+  $('.container-data').on('click', '.display-data-item', function(e){ // delete item
     console.log(e.currentTarget.dataset.keyvalue);
     var keyData = e.currentTarget.dataset.keyvalue;
     localStorage.removeItem(keyData);
     $('.container-data').text('');
   });
-  // delete all?
-  $('.btn-clear').click(function(){
+
+  $('.btn-clear').click(function(){ // delete all?
     localStorage.clear();
     $('.container-data').text('');
   });
---
+++ app/tonegen.js
@@ -0,0 +1,37 @@
+var notesheet = {
+  'c': 261.6,
+  'd': 293.7,
+  'e': 329.6,
+  'f': 349.2,
+  'g': 392.0,
+  'a': 440.0,
+  'b': 493.9
+}
+
+var soundbank = {};
+
+var Tonematrix = function (length, notesheet) {
+  this.sequenceLength = length;
+  this.notesheet = notesheet;
+  this.reset();
+}
+
+Tonematrix.prototype.reset = function () {
+  for (let note in this.notesheet) {
+    this[note] = [...new Array(this.sequenceLength)].map(x => 0);
+  }
+}
+
+Tonematrix.prototype.eachStep = function (callback) {
+  for (let step = 0; step < this.sequenceLength; step++) {
+    for (let note in this.notesheet) {
+      callback(this[note][step], step, note, this[note], this);
+    }
+  }
+}
+
+Tonematrix.prototype.eachNote = function (callback) {
+  for (let note in this.notesheet) {
+    callback(note, this.notesheet[note], this[note], this);
+  }
+}
